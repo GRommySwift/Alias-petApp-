f03f539f1da72469c0cee53f677b4a211cd569a0
GRoMMy <romaholovai@gmail.com>
2023-09-10 16:07:29 +0200

    Initial Commit

---
+++ Alias/AliasApp.swift
@@ -0,0 +1,17 @@
+//
+//  AliasApp.swift
+//  Alias
+//
+//  Created by Roman on 10/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct AliasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
