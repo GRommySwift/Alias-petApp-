11efba28f6101fa80d0b6dc29dd72da3339ef441
GRoMMy <romaholovai@gmail.com>
2023-10-02 10:56:37 +0200

    added card with words, but need to work with it

---
+++ Alias/View/GameScreen/Game/CardContent.swift
@@ -0,0 +1,18 @@
+//
+//  CardContent.swift
+//  Alias
+//
+//  Created by Roman on 02/10/2023.
+//
+
+import SwiftUI
+
+struct CardContent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardContent()
+}
